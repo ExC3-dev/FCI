cf997c364809309e3273610b53e8f55d6f242b8f
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-02 09:59:04 -0900

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,14 @@
+/// Main.js
+document.addEventListener("keydown", function (e) {
+	if (e.key == "~" && e.ctrlKey) {
+		var t = window.open("", "_blank", "width=500,height=100");
+		var e = t.document.createElement("iframe");
+		(e.src = "//ustuff.github.io/uRun/popup.html"),
+			(e.style.cssText = "width:100%; height:100%; border:none;"),
+			t.document.body.appendChild(e),
+			t.document.title = "FCI",
+			t.addEventListener("message", function (e) {
+				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+			});
+	}
+});
