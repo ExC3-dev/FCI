0559577dc8640144f9ee13f93149d985fc4a1ede
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-02 10:00:38 -0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=100");
 		var e = t.document.createElement("iframe");
-		(e.src = "//ustuff.github.io/uRun/popup.html"),
+		(e.src = "//raw.githubusercontent.com/ExC3-dev/FCI/refs/heads/main/GUI.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "FCI",
