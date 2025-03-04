6c74b1a985667defdbf4d9e75112a34b2ebc7cfc
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-03 16:01:00 -0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,7 +4,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=100");
 		var e = t.document.createElement("iframe");
-		(e.src = "//wondrous-boba-db8342.netlify.app"),
+		(e.src = "//fci.pages.dev"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "FCI",
