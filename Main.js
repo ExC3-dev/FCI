8c466c8b681a704082b5787bdfc8b57e6c7b8321
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-04 18:33:32 -0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "//fcihtml.w3spaces.com"),
+		(e.src = "//fcihtml.w3spaces.com/#"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "Fci",
