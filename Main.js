0b98bf75983d62ce7ba632498326808b0bda1c1f
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-05 20:10:46 -0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-/// urun.js
+/// Main.js
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
@@ -6,7 +6,7 @@ document.addEventListener("keydown", function (e) {
 		(e.src = "//edt-fci.staticrun.app"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
-			t.document.title = "uRun",
+			t.document.title = "Fci",
 			t.addEventListener("message", function (e) {
 				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
