5261f771ed9d52a0ed7efa04e4505ca7956b22a8
ExC3 <Iyoukeen.crass@gmail.com>
2025-03-06 18:54:33 -0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,14 +1,38 @@
 /// Main.js
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//edt-fci.staticrun.app"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});
+		// Create a floating div
+		var floatingDiv = document.createElement("div");
+		floatingDiv.style.cssText = "position:fixed; top:50px; left:50px; width:500px; height:300px; border:1px solid #000; background-color:#fff; z-index:1000;";
+
+		// Create an iframe
+		var iframe = document.createElement("iframe");
+		iframe.src = "//edt-fci.staticrun.app";
+		iframe.style.cssText = "width:100%; height:calc(100% - 30px); border:none;";
+
+		// Create a close button
+		var closeButton = document.createElement("button");
+		closeButton.innerText = "Close";
+		closeButton.style.cssText = "position:absolute; top:0; right:0;";
+
+		// Add functionality to the close button
+		closeButton.addEventListener("click", function () {
+			document.body.removeChild(floatingDiv);
+		});
+
+		// Append iframe and close button to the floating div
+		floatingDiv.appendChild(iframe);
+		floatingDiv.appendChild(closeButton);
+
+		// Append the floating div to the body
+		document.body.appendChild(floatingDiv);
+
+		// Listen for messages from the iframe
+		window.addEventListener("message", function (e) {
+			if (e.data.toString().startsWith("execute:")) {
+				eval(e.data.toString().replace("execute:", ""));
+				document.body.removeChild(floatingDiv);
+			}
+		});
 	}
 });
